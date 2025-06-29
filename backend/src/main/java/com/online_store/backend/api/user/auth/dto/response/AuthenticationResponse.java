cb585c46be64e7e90d1e42cadb89fe371a0a58db
pelittomer <pelittomer99@gmail.com>
2025-06-29 17:12:21 +0300

    feat: Create Authentication and Error response DTOs.

---
+++ backend/src/main/java/com/online_store/backend/api/user/auth/dto/response/AuthenticationResponse.java
@@ -0,0 +1,18 @@
+package com.online_store.backend.api.user.auth.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class AuthenticationResponse {
+    private String accessToken;
+    private String refreshToken;
+}
